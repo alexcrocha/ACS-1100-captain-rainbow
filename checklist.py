200176139b465ae1cb3a55f4ea601ea5bcf05b13
Alex Rocha <rocha.amc@gmail.com>
2022-09-19 21:49:17 -0700

    Create main loop and add exit option

--- checklist.py
+++ checklist.py
@@ -1,3 +1,16 @@
 colours = ['red', 'orange', 'yellow', 'green', 'blue', 'indigo', 'violet']
 
 clothes = ['left shoe', 'right shoe', 'pants', 'belt', 'top', 'mask', 'cape']
+
+def select(option):
+  if option == 'q':
+    return False
+  return True
+
+running = True
+
+while running:
+  selection = input(
+    'Press Q to exit\n'
+  )
+  running = select(selection)
