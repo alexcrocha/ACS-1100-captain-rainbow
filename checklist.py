a1977222b21905fbf246ad930aa1c271b64160dd
Alex Rocha <rocha.amc@gmail.com>
2022-09-19 21:51:23 -0700

    Fix indentation

--- checklist.py
+++ checklist.py
@@ -1,16 +1,16 @@
-colours = ['red', 'orange', 'yellow', 'green', 'blue', 'indigo', 'violet']
+colours = ["red", "orange", "yellow", "green", "blue", "indigo", "violet"]
 
-clothes = ['left shoe', 'right shoe', 'pants', 'belt', 'top', 'mask', 'cape']
+clothes = ["left shoe", "right shoe", "pants", "belt", "top", "mask", "cape"]
+
 
 def select(option):
-  if option == 'q':
-    return False
-  return True
+    if option == "q":
+        return False
+    return True
+
 
 running = True
 
 while running:
-  selection = input(
-    'Press Q to exit\n'
-  )
-  running = select(selection)
+    selection = input("Press Q to exit\n")
+    running = select(selection)
