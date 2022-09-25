adcfdeb6f6a04817b8199d7fe40332b6da7dc0ae
Alex Rocha <rocha.amc@gmail.com>
2022-09-25 15:28:17 -0700

    Remove import

--- checklist.py
+++ checklist.py
@@ -1,6 +1,3 @@
-from tabnanny import check
-
-
 colours = ["red", "orange", "yellow", "green", "blue", "indigo", "violet"]
 clothes = ["left shoe", "right shoe", "pants", "belt", "top", "mask", "cape"]
 
