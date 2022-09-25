285f883767e940886d9ffafc77ee22c4ca371255
Alex Rocha <rocha.amc@gmail.com>
2022-09-25 15:19:24 -0700

    Add CRUD functions

--- checklist.py
+++ checklist.py
@@ -1,10 +1,40 @@
-colours = ["red", "orange", "yellow", "green", "blue", "indigo", "violet"]
+from tabnanny import check
 
+
+colours = ["red", "orange", "yellow", "green", "blue", "indigo", "violet"]
 clothes = ["left shoe", "right shoe", "pants", "belt", "top", "mask", "cape"]
+
+checklist = list()
+
+
+def create(item):
+    checklist.append(item)
+
+
+def read(index):
+    return checklist[index]
+
+
+def update(index, item):
+    checklist[index] = item
+
+
+def destroy(index):
+    checklist.pop(index)
 
 
 def select(option):
-    if option == "q":
+    if option == "c":
+        create(input("Which item would you like to add? > "))
+    elif option == "r":
+        print(read(int(input("Which index? > "))))
+    elif option == "u":
+        index = int(input("Which index do you want to replace? > "))
+        item = input("Which item would you like to add? > ")
+        update(index, item)
+    elif option == "d":
+        destroy(int(input("Which index do you want to delete? > ")))
+    elif option == "q":
         return False
     return True
 
@@ -12,5 +42,7 @@ def select(option):
 running = True
 
 while running:
-    selection = input("Press Q to exit\n")
+    selection = input(
+        "Select one of the following options:\nC to add to the list\nR to read the list\nU to update the list\nD to delete an item from the list\nQ to exit\n"
+    )
     running = select(selection)
