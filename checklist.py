919247c1df560b199dbd0607f69ae0ac084b935e
Alex Rocha <rocha.amc@gmail.com>
2022-09-25 22:18:07 -0700

    Add mark complete and user input functions

--- checklist.py
+++ checklist.py
@@ -26,28 +26,12 @@ def list_all_items():
         print("{} {}".format(index, list_item))
         index += 1
 
-
-def test():
-    create("purple sox")
-    create("red cloak")
-
-    print(read(0))
-    print(read(1))
-
-    update(0, "purple socks")
-    destroy(1)
-
-    print(read(0))
-
-    list_all_items()
-
-
-test()
-
+def mark_complete(index):
+    checklist[index] = '√' + checklist[index]
 
 def select(option):
     if option == "c":
-        create(input("Which item would you like to add? > "))
+        create(input("Input item > "))
     elif option == "r":
         valid_index = False
         while valid_index == False:
@@ -61,6 +45,15 @@ def select(option):
         index = int(input("Which index do you want to replace? > "))
         item = input("Which item would you like to add? > ")
         update(index, item)
+    elif option == "v":
+        valid_index = False
+        while valid_index == False:
+            item_index = int(input("Index Number? > "))
+            if len(checklist) > item_index:
+                valid_index = True
+            else:
+                print(f"Index has to be lower than {len(checklist)}")
+        mark_complete(item_index)
     elif option == "a":
         list_all_items()
     elif option == "d":
@@ -70,10 +63,38 @@ def select(option):
     return True
 
 
+def user_input(prompt):
+    user_input = input(prompt)
+    return user_input
+
+
+def test():
+    create("purple sox")
+    create("red cloak")
+
+    print(read(0))
+    print(read(1))
+
+    update(0, "purple socks")
+    destroy(1)
+
+    print(read(0))
+
+    select("c")
+
+    list_all_items()
+
+    select("r")
+
+    list_all_items()
+
+
+test()
+
 running = True
 
 while running:
-    selection = input(
-        "Select one of the following options:\nC to add to the list\nR to read the list\nU to update the list\nD to delete an item from the list\nA to list all items\nQ to exit\n"
+    selection = user_input(
+        "Select one of the following options:\nC to add to the list\nR to read the list\nU to update the list\nD to delete an item from the list\nV to mark an item complete\nA to list all items\nQ to exit\n"
     )
     running = select(selection)
